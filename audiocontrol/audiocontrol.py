69b61ccd7b8b00f984ec9e33f3932807d0815b29
EzFeDezy <isaias.dietz@gmx.de>
2022-08-18 02:22:27 +0200

    basic requirements

--- audiocontrol/audiocontrol.py
+++ audiocontrol/audiocontrol.py
@@ -1,4 +1,8 @@
 import pulsectl
+import sys
+sys.path.append("..")
+from log.log import *
+
 """
 Commands:
 listsources
@@ -14,5 +18,21 @@ decvolume           sink/source, id, value
 getmute             sink/source, id
 setmute             sink/source, id, 1/0
 togglemute          sink/source, id
-
 """
+
+
+class audiocontrol(object):
+
+    def __init__(self):
+        global pulse
+        initLog("audiocontrol")
+        pulse = pulsectl.Pulse("audiocontrol")
+    
+    def initcore(self, out_q, in_q):
+        global queue_out, queue_in
+        queue_out = out_q
+        queue_in = in_q
+    
+    def run(self):
+        log("running")
+        
