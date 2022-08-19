492696a9e59d1b66697a826a52baeb769a9b9a92
EzFeDezy <isaias.dietz@gmx.de>
2022-08-19 22:59:09 +0200

    smol uppsie

--- audiocontrol/audiocontrol.py
+++ audiocontrol/audiocontrol.py
@@ -5,6 +5,7 @@ import pulsectl
 import sys
 sys.path.append("..")
 from log.log import *
+from conmanager.utils import *
 
 """
 Commands:
@@ -43,46 +44,9 @@ class audiocontrol(object):
         queue_out = out_q
         queue_in = in_q
     
-    def parse(read):
-        if read[0] == "conmanager": #External packet
-            #Import the data. 
-            data = eval(read[1][3], {'__builtins__': None}) #'{__builtins': None} so that you can't execute code. Just strings and stuff.
-            external = True
-            orig_device = read[1][1]
-            orig_module = read[1][2]
-            try: #Check for packet_id
-                packet_id = read[1][4]
-            except:
-                packet_id = None
-        else: #Internal packet
-            external = False
-            orig_device = None
-            orig_module = read[0]
-            try: #Check for packet_id
-                packet_id = read[2]
-            except:
-                packet_id = None
 
-        if type(read[1]) == list or type(read[1]) == tuple:
-            action = read[1][0]
-            params = read[1][1:]
-        else:
-            action = read[1][0]
-            params = []
-        return {"external": external, "origdevice": orig_device, "origmodule": orig_module, "action": action, "params": params}
-
-    def respond(read, msg):
-        if read.external: #External packet
-            #conmanager ---> audiocontrol: ("recvdata", orig_device, orig_modname, data)
-            if type(msg) != bytes:
-                msg = bytes(msg, "utf-8")
-            queue_out.put(("conmanager", ("senddata", read.orig_device, read.orig_modname, msg, read.packet_id)))
-        else: #Internal packet
-            queue_out.put((read.orig_modname, msg, read.packet_id)) #Put msg in additional parentheses or not? I'd say no.
-    
     def run(self):
         global pulse
-        global respond, parse #For visual studio to shut up about warnings
         log("running")
         time.sleep(0.05)
         if not queue_in.empty():
