0ba89f813740b80fae3228153df464332689ec0f
EzFeDezy <isaias.dietz@gmx.de>
2022-09-26 21:27:17 +0200

    GUI generation now working

--- hwdisplay/hwdisplay.py
+++ hwdisplay/hwdisplay.py
@@ -1,13 +1,18 @@
+from email import header
 from email.errors import FirstHeaderLineIsContinuationDefect
 from http.client import SWITCHING_PROTOCOLS
+from importlib.resources import path
+from multiprocessing.connection import PipeConnection
 import os
 import sys
 import time
+import queue
 from uuid import getnode
 import xml.dom.minidom
 from datetime import datetime, timedelta
+#dependency PIL?
+from PIL import ImageTk, Image
 
-from symbol import try_stmt
 
 #NO!    #dependency: pip3 install tkhtmlview
 #       #from tkhtmlview import HTMLLabel
@@ -15,8 +20,10 @@ from symbol import try_stmt
 #                               #from cefpython3 import cefpython as cef
 #dependency: pip install tkinterhtml
 #from tkinterhtml import HTMLFrame
+#Screw HTML. I'm doing tkinter + XML manually.
 
 sys.path.append("..")
+sys.path.append("hwdisplay")
 import importlib
 import threading
 import time
@@ -44,7 +51,7 @@ class RepeatEvery(threading.Thread):
     def stop(self):
         self.runable = False
 
-class hwdisplay(object):
+class windowmanager(object):
 
     #def findNode(self, pNode, pName):
     #    for child in pNode.childNodes:
@@ -57,14 +64,18 @@ class hwdisplay(object):
                 return child
 
     def getValues(self, pNode):
+        #print("DEBUG@getValues: entry {}".format(pNode.nodeName))
         retValues = {"attributes": {}, "childNodes": []}
         for element in pNode.childNodes:
             if element == None:
                 continue
             #Problem: Text between tags is its own element. even "\n\n   "... FML. <tag>text<tag2>moretext</tag2>evenmoretext</tag>
-            retValues["childNodes"].append(self.getValues(pNode))
-        for attribute in pNode.attributes.values():
-            retValues["attributes"][attribute.name] = attribute.value
+            retValues["childNodes"].append(self.getValues(element))
+        try: #If has attributes:
+            for attribute in pNode.attributes.values():
+                retValues["attributes"][attribute.name] = attribute.value
+        except: #else: do nothing.
+            pass
         try:
             retValues["name"] = retValues["attributes"]["name"]
         except:
@@ -73,36 +84,130 @@ class hwdisplay(object):
             retValues["attributes"]["page"]
         except:
             retValues["attributes"]["page"] = None
-        retValues["tagName"] = pNode.tagName
+        retValues["nodeName"] = pNode.nodeName
         retValues["nodeValue"] = pNode.nodeValue
         retValues["self"] = pNode
+        #print("DEBUG@getValues: end")
         return retValues
     
-    def loadElement(self, pGui, pElement):
-        match pElement["tagName"]:
+    def getVariable(self, pDisplay, pName):
+        return pDisplay["variables"][pName]
+    
+    def hasAttribute(self, pElement, pAttribute):
+        try:
+            print("DEBUG@hasAttribute: try")
+            print("DEBUG@hasAttribute: pElement {}".format(pElement))
+            print("DEBUG@hasAttribute: attributes {}".format(pElement["attributes"]))
+
+            pElement["attributes"][pAttribute]
+            return True
+        except:
+            return False
+
+    
+    def loadElement(self, pDisplay, pElement):
+        print("DEBUG@loadElement: entry pElement {}".format(pElement))
+
+        element = {}
+        match pElement["nodeName"]:
             case "pagecontroller":
+                print("DEBUG@loadElement: pagecontroller {}|{}".format(pDisplay["window"], pElement))
                 pass #Todo
             case "label":
-                pass #Todo
+                print("DEBUG@loadElement: label")
+                element["type"] = "label"
+                x = pElement["attributes"]["x"]
+                y = pElement["attributes"]["y"]
+                try:
+                    try:
+                        text = pElement["childNodes"][0]["nodeValue"]
+                    except:
+                        text = pElement["attributes"]["text"]
+                    element["handle"] = tk.Label(pDisplay["window"], text = text)
+                    print("DEBUG@loadElement: label text {} x {} y {}".format(text, x, y))
+                except: #if has variable="variablename"
+                    textvariable = pDisplay["variables"][pElement["attributes"]["variable"]]["variable"]
+                    element["handle"] = tk.Label(pDisplay["window"], textvariable = textvariable)
+                    print("DEBUG@loadElement: label textvariable {} x {} y {}".format(textvariable.get(), x, y))
+                
+                
+                #element["handle"].pack()
+                element["handle"].place(x = x, y = y)
+                print("DEBUG@loadElement: label placed {} x {} y {}".format(element["handle"].place_info(), element["handle"].winfo_x, element["handle"].winfo_y))
             case "image":
-                pass #Todo
+                print("DEBUG@loadElement: image")
+                element["type"] = "label"
+                x = pElement["attributes"]["x"]
+                y = pElement["attributes"]["y"]
+                path = "{}/{}".format(pDisplay["fullbasepath"], pElement["attributes"]["image"])
+                print("DEBUG@loadElement: image path {}".format(path))
+                element["image"] = ImageTk.PhotoImage(file = path)
+                element["handle"] = tk.Label(pDisplay["window"], image = element["image"])
+                element["handle"].place(x = x, y = y)
+
             case "variable":
-                pass #Todo
+                print("DEBUG@loadElement: variable")
+                temp = {}
+                temp["variable"] = StringVar(pDisplay["window"])
+                temp["variable"].set("DEBUGSTRING") #DEBUG #todo
+                temp["every"] = pElement["attributes"]["every"]
+                temp["function"] = eval('pDisplay["controller"].{}'.format(pElement["attributes"]["function"]))
+#                print("DEBUG@loadElement: variablefunction {}".format(temp["function"](temp["variable"])))
+                temp["repeatEvery"] = RepeatEvery(float(temp["every"]), temp["function"], temp["variable"])
+                temp["repeatEvery"].start()
+                pDisplay["variables"][pElement["name"]] = temp
             case "canvas":
+                print("DEBUG@loadElement: canvas")
                 pass #Todo
+            case _:
+                print("something else. {}".format(pElement))
+            
+        try: #postconfig
+            print("DEBUG@loadElement: try")
+            if self.hasAttribute(pElement, "color"):
+                print("DEBUG@loadElement: color")
+                element["handle"].config(fg = pElement["attributes"]["color"])
+            elif self.hasAttribute(pElement, "colour"):
+                print("DEBUG@loadElement: colour")
+                element["handle"].config(fg = pElement["attributes"]["colour"])
+            elif self.hasAttribute(pElement, "fg"):
+                print("DEBUG@loadElement: fg")
+                element["handle"].config(fg = pElement["attributes"]["fg"])
+            else:
+                element["handle"].config(fg = pDisplay["head"]["fg"])
+            if self.hasAttribute(pElement, "bgcolor"):
+                print("DEBUG@loadElement: bgcolor")
+                element["handle"].config(bg = pElement["attributes"]["bgcolor"])
+            elif self.hasAttribute(pElement, "bgcolour"):
+                print("DEBUG@loadElement: bgcolour")
+                element["handle"].config(bg = pElement["attributes"]["bgcolour"])
+            elif self.hasAttribute(pElement, "bg"):
+                print("DEBUG@loadElement: bg")
+                element["handle"].config(bg = pElement["attributes"]["bg"])
+            else:
+                element["handle"].config(bg = pDisplay["head"]["bg"])
+        except Exception as e:
+            print("DEBUG@loadElement: ERROR {}".format(e))
+            pass
+        return element
+        print("DEBUG@loadElement: end")
 
 
     def loadPage(self, pDisplay, pPage):
+        print("DEBUG@loadPage: entry pPage: {}".format(pPage))
         elementtypes = {""}
         for element in pDisplay["body"]["childNodes"]:
-            if element["attributes"]["page"] == pPage or element["attributes"]["page"] == None:
-                self.loadElement(self, pDisplay["window"], element)
+            if element["nodeName"] != "#text" and (element["attributes"]["page"] == pPage or element["attributes"]["page"] == None):
+                loadedElement = self.loadElement(pDisplay, element)
+                pDisplay["pages"][pPage].append(loadedElement)
+        print("DEBUG@loadPage: end")
 
     def getCurrentPage(self, display):
+        print("DEBUG@getCurrentPage: entry")
         if display["pages"] == {}:
             display["pages"]["current"] = None
             for element in display["body"]["childNodes"]:
-                if element["tagName"] == "pagecontroller":
+                if element["nodeName"] == "pagecontroller":
                     try:
                         display["pages"]["current"] = element["attributes"]["default"]
                     except:
@@ -115,23 +220,64 @@ class hwdisplay(object):
             raise Exception("Formatting error: Please make sure you have a page controller containing pages")
 
     def initDisplay(self, pDisplay):
+        print("DEBUG@initDisplay: entry")
         pDisplay["collection"] = self.getValues(pDisplay["collection"])
         pDisplay["window"] = tk.Tk()
-        pDisplay["head"] = self.getNode(self, pDisplay["collection"], "head")
-        pDisplay["body"] = self.getnode(self, pDisplay["collection"], "body")
+        print("DEBUG@initDisplay: window {}".format(pDisplay["window"].winfo_geometry()))
+        pDisplay["head"] = self.getNode(pDisplay["collection"], "head")
+        pDisplay["body"] = self.getNode(pDisplay["collection"], "body")
+        pDisplay["variables"] = {}
         pDisplay["pages"] = {}
         for element in pDisplay["head"]["childNodes"]:
-            pDisplay["head"][element["name"]] = element["childNodes"][0]["nodeValue"] #To get text from <tag>text</tag>, get it's 0-th child's (aka. text node) value
-        pDisplay["window"].geometry("%dx%d+%d+%d" % (pDisplay["head"]["width"], pDisplay["head"]["height"], pDisplay["head"]["x"], pDisplay["head"]["y"]))
-        #todo: .config(bg=backgroundcolor)
-        #todo: Attributes like: .attributes('-fullscreen', True) or .wm_attributes('-topmost', True)
-        #todo: .title("user selected title")
-        self.loadPage(self, pDisplay self.getCurrentPage(self, pDisplay))
+            if element["name"] != "#text":
+                pDisplay["head"][element["name"]] = element["childNodes"][0]["nodeValue"] #To get text from <tag>text</tag>, get it's 0-th child's (aka. text node) value
+#        print("DEBUG@initDisplay: basepath {}".format(pDisplay["basepath"]))
+#        print("DEBUG@initDisplay: controller {}".format(pDisplay["head"]["controller"]))
+#        print("DEBUG@initDisplay: full path {}".format("{}.{}".format(pDisplay["basepath"], pDisplay["head"]["controller"]).replace("/", ".")))
+#        print("DEBUG@initDisplay: current path {}".format(sys.path))
+        pDisplay["controller"] = importlib.import_module("{}.{}".format(pDisplay["basepath"], pDisplay["head"]["controller"]).replace("/", "."))
+        pDisplay["controllerIn"] = queue.Queue()
+        pDisplay["controllerOut"] = queue.Queue()
+        pDisplay["controller"].initController(pDisplay["controllerIn"], pDisplay["controllerOut"])
+#        print("DEBUG@initDisplay: window generation:")
+#        print("{}x{}+{}+{}".format(pDisplay["head"]["width"], pDisplay["head"]["height"], pDisplay["head"]["x"], pDisplay["head"]["y"]))
+        pDisplay["window"].wm_geometry("{}x{}+{}+{}".format(pDisplay["head"]["width"], pDisplay["head"]["height"], pDisplay["head"]["x"], pDisplay["head"]["y"]))
         
+        try:
+            pDisplay["window"].title(pDisplay["head"]["title"])
+        except:
+            pass
+
+        try:
+            pDisplay["window"].config(bg=pDisplay["head"]["bg"])
+        except:
+            pDisplay["window"].config(bg="#000000")
+
+        try:
+            pDisplay["head"]["topmost"] #If this tag exists, make window topmost.
+            pDisplay["window"].wm_attributes('-topmost', True)
+        except:
+            pass
+
+        try:
+            pDisplay["head"]["fullscreen"] #If this tag exists, make window full screen
+            pDisplay["window"].attributes('-fullscreen', True)
+        except:
+            pass
+#        print("DEBUG@initDisplay: window {}".format(pDisplay["window"].winfo_geometry()))
+
+        pDisplay["window"].update()
+        print("DEBUG@initDisplay: before loadPage")
+        self.loadPage(pDisplay, self.getCurrentPage(pDisplay))
+        print("DEBUG@initDisplay: after loadPage")
+        pDisplay["loaded"] = 1
+        pDisplay["window"].mainloop()
+
+class hwdisplay(object):
 
     def __init__(self):
         initLog("hwdisplay")
-        global packetID
+        global packetID, VERSION, basepath, config, displays
         packetID = 0
         
         global VERSION, MONITOR, config, window, window_x, window_y, window_w, window_h, canvas
@@ -141,11 +287,19 @@ class hwdisplay(object):
         config.read(basepath + '/config.ini')
         displays = []
         for display in config["displays"]:
+            print("DEBUG@__init__: display {}".format(display))
             tempDisplay = {}
-            tempDisplay["basepath"] = basepath + '/' + display
-            tempDisplay["DOMTree"] = xml.dom.minidom.parse(tempDisplay["basepath"] + '/' + display + '.xml')
+            tempDisplay["fullbasepath"] = basepath + '/' + display
+            tempDisplay["basepath"] = display
+            tempDisplay["DOMTree"] = xml.dom.minidom.parse(tempDisplay["fullbasepath"] + '/' + display + '.xml')
             tempDisplay["collection"] = tempDisplay["DOMTree"].documentElement
-            displays.append(display)
+            tempDisplay["loaded"] = 0
+            wm = windowmanager() # I have to put the whole GUI stuff into a separate thread because tkinter requires tk.Tk() and mainloop() to be executed in the same thread.
+            tempDisplay["mainloop"] = threading.Thread(target=wm.initDisplay, args=(tempDisplay,))
+            tempDisplay["mainloop"].start()
+#            tempDisplay["window"].mainloop()
+            displays.append(tempDisplay)
+            print("DEBUG@__init__: display END")
         #
         #COLOR_BG = config.get("colors", "bg")
         #COLOR_FG = config.get("colors", "fg")
@@ -195,7 +349,15 @@ class hwdisplay(object):
         pass
 
     def run(self):
-        pass
+        while True:
+            time.sleep(0.01)
+            if not inQueue.empty(): # Intermetry ---> core ---> hwdisplay [todo: ---> displays]
+                pass
+            for display in displays:
+                if not display["controllerOut"].empty():
+                    read = display["controllerOut"].get()
+                    
+
         #updateThread = RepeatEvery(0.5, self.update, (window, canvas))
         #updateThread.start()
         
