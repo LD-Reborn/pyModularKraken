d65ae4518ee9def8a9c2d86aa441894a805f2fad
EzFeDezy <isaias.dietz@gmx.de>
2022-09-27 12:56:28 +0200

    added functionality

--- hwdisplay/hexa/hexa.py
+++ hwdisplay/hexa/hexa.py
@@ -1,5 +1,3 @@
-from cmath import pi
-from sys import displayhook
 import time
 import psutil
 import netifaces
@@ -11,12 +9,32 @@ import random
 
 usageramps=("▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", "█") #<--- Duplicate at the end to prevent tuple index out of range error, while keeping maths simple / reducing number of instructions 
 
-def initController(pInQueue, pOutQueue, pDisplay):
-	global inQueue, outQueue, display
+def initController(pInQueue, pOutQueue, pDisplay, pTarget):
+	global inQueue, outQueue, display, target, packets
 	inQueue = pInQueue
 	outQueue = pOutQueue
 	display = pDisplay
+	target = pTarget
+	packets = []
 
+def requestalldata(pVar):
+	global inQueue, outQueue, display, target, packets
+	#read = {dstModule: "destination module", dstDevice: "destination device", data: "data"}
+	id = random.randbytes(8)
+	#Currently implemented:
+	#	"cpu" "cpu_all" "cpu_numcores"
+	#	"ram_percent" "ram_total" "ram_used"
+	#	"gpu_name" "gpu_temp" "gpu_utilization" "gpu_memused" "gpu_memtotal" "gpu_memusedPercent"
+    #	"nic_address" "nic_io" "nic_linkspeed" "nic_mtu" "nic_isup"
+	request = "cpu,cpu_all,ram_percent,ram_total,gpu_utilization,gpu_memused,gpu_memusedPercent,gpu_temp,nic_address,nic_io,nic_linkspeed,nic_mtu,nic_isup"
+	packets.append(id)
+	outQueue.put({"dstModule": "intermetry", "dstDevice": target, "data": "hardwareinfo:{}:{}".format(id, request)})
+	if not inQueue.empty():
+		read = inQueue.read()
+		if len(packets) > 0:
+			#try:
+			#	packets.index()
+			print(read)
 def cpupercent(pVar):
 	print("debug@cpupercent: WIP")
 #	ramps = ""
