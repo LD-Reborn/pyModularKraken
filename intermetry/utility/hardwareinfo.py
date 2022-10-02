dda3948b67995a7e27730d302ae69e718e771b68
EzFeDezy <isaias.dietz@gmx.de>
2022-10-02 03:08:48 +0200

    performance improvement

--- intermetry/utility/hardwareinfo.py
+++ intermetry/utility/hardwareinfo.py
@@ -4,16 +4,22 @@ import GPUtil
 import sys
 sys.path.append("../..")
 from log.log import *
+import time # for debugging
 
 def parseRequest(pText):
+    global gpus
     funcmap = {"cpu": cpu, "cpu_all": cpu_all, "cpu_numcores": cpu_numcores, "ram_percent": ram_percent, "ram_total": ram_total, "ram_used": ram_used, "gpu_name": gpu_name, "gpu_temp": gpu_temp, "gpu_utilization": gpu_utilization, "gpu_memused": gpu_memused, "gpu_memtotal": gpu_memtotal, "gpu_memusedPercent": gpu_memusedPercent, "nic_address": nic_address, "nic_io": nic_io, "nic_linkspeed": nic_linkspeed, "nic_mtu": nic_mtu, "nic_isup": nic_isup}
     requests = pText.split(",")
     returnstring = ""
+    if pText.find("gpu") > -1: # performance optimization
+        gpus = GPUtil.getGPUs() # Takes around 0.1 seconds to execute. Probably because it interfaces to nvidia-smi.
     for request in requests:
+        time1 = time.time()
         try:
             returnstring += "{}|".format(funcmap[request]())
         except Exception as msg:
             errout("HARDWAREINFO: unable to parse requested hardware info {}. Error: {}".format(request, msg))
+        print("DEBUG@hardwareinfo request time {} for request {}".format(time.time() - time1, request))
     return returnstring
 
 #CPU usage in percent
@@ -44,42 +50,42 @@ def ram_used():
 #GPU name
 def gpu_name():
     returnstring = ""
-    for gpu in GPUtil.getGPUs():
+    for gpu in gpus:
         returnstring += "{},".format(gpu.name)
     return returnstring[:-1]
 
 #GPU temperature in °C
 def gpu_temp():
     returnstring = ""
-    for gpu in GPUtil.getGPUs():
+    for gpu in gpus:
         returnstring += "{},".format(gpu.temperature)
     return returnstring[:-1]
 
 #GPU compute utilization in percent
 def gpu_utilization():
     returnstring = ""
-    for gpu in GPUtil.getGPUs():
+    for gpu in gpus:
         returnstring += "{},".format(gpu.load * 100)
     return returnstring[:-1]
 
 #Used GPU memory in MB
 def gpu_memused():
     returnstring = ""
-    for gpu in GPUtil.getGPUs():
+    for gpu in gpus:
         returnstring += "{},".format(gpu.memoryUsed)
     return returnstring[:-1]
 
 #Total GPU memory in MB
 def gpu_memtotal():
     returnstring = ""
-    for gpu in GPUtil.getGPUs():
+    for gpu in gpus:
         returnstring += "{},".format(gpu.memoryTotal)
     return returnstring[:-1]
 
 #Used GPU memory in percent
 def gpu_memusedPercent():
     returnstring = ""
-    for gpu in GPUtil.getGPUs():
+    for gpu in gpus:
         returnstring += "{},".format(gpu.memoryUsed / gpu.memoryTotal * 100)
     return returnstring[:-1]
 
