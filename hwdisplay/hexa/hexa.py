906141306738b2d63cbf9cbe1d3c412f5d2cede4
EzFeDezy <isaias.dietz@gmx.de>
2022-11-22 04:57:49 +0100

    more displayed info

--- hwdisplay/hexa/hexa.py
+++ hwdisplay/hexa/hexa.py
@@ -31,35 +31,18 @@ def addDatapoint(pName, pValue):
 def requestalldata(pVar):
 	time1 = time.time()
 	print("debug@requestalldata: entry")
-	#global inQueue, outQueue, display, target, packets, sensors_temperatures, sensors_fans, cpu, cpu_all, ram_percent, ram_total, gpu_utilization, gpu_memused, gpu_memusedPercent, gpu_memtotal, gpu_temp, nic_address, nic_io, nic_linkspeed, nic_mtu, nic_isup
 	global inQueue, outQueue, display, target, packets, values
 	values = {}
-	#cpu = None # Define early to prevent access on undefined variable. 
-	#cpu_all = []
-	#ram_percent = None
-	#ram_total = None
-	#gpu_utilization = None
-	#gpu_memused = None
-	#gpu_memusedPercent = None
-	#gpu_temp = None
-	#nic_address = None
-	#nic_io = None
-	#nic_linkspeed = None
-	#nic_mtu = None
-	#nic_isup = None
-	#read = {dstModule: "destination module", dstDevice: "destination device", data: "data"}
 	id = random.randbytes(4)
 	# For a list of currently implemented requestable datapoints, see hardwareinfo.py
-	request = "cpu|cpu_all|ram_percent|ram_total|gpu|nic_address|nic_io|nic_linkspeed|nic_mtu|nic_isup|sensors.temp|sensors.fan"
+	request = "cpu|cpu_all|ram_percent|ram_used|ram_total|gpu|nic_address|nic_io|nic_linkspeed|nic_mtu|nic_isup|sensors.temp|sensors.fan|sensors.power"
 	packets.append(id)
-	# outQueue.put makes CPU utilization on all cores rise to 25% if start.py is run in VS code. In terminal this does not happen.
+	# outQueue.put makes CPU utilization on all cores rise to 25% if start.py is run in VS code. In terminal this does not happen. Probably because VS code runs as a snap.
 	outQueue.put({"dstModule": "intermetry", "dstDevice": target, "data": "hardwareinfo:{}".format(request), "packetID": id})
 	while not inQueue.empty():
 		time.sleep(0.01)
 		read = inQueue.get()
 		if len(packets) > 0:
-			#try:
-			#	packets.index()
 			if read[0] == "conmanager" and read[1][0] == "recvdata" and read[1][1] == target:
 				try:
 					split = read[1][3].decode("utf-8").split(":")
@@ -70,39 +53,9 @@ def requestalldata(pVar):
 						data = json.loads(read[1][3][13:].decode("utf-8"))
 				except Exception as e:
 					continue
-				print("DEBUG @ hexa -----")
-				print(data)
-				print("-----")
 				for element in data:
 					addDatapoint(element, data[element])
 					values[element] = data[element]
-					
-				#cpu = data["cpu"] #data[0]
-				#cpu_all = data["cpu_all"]#data[1][1:-1].split(", ")
-				#ram_percent = data["ram_percent"]#data[2]
-				#ram_total = data["ram_total"]#data[3]
-				#gpu_utilization = data[""]#data[4]
-				#gpu_memused = #data[5]
-				#gpu_memusedPercent = #data[6]
-				#gpu_memtotal = #data[7]
-				#gpu_temp = #data[8]
-				#nic_address = #data[9]
-				#nic_io = #data[10]
-				#nic_linkspeed = #data[11]
-				#nic_mtu = #data[12]
-				#nic_isup = #data[13]
-				#sensors_temperatures = data[14]
-				#sensors_fans = data[15]
-				#addDatapoint("cpu", cpu)
-				#addDatapoint("gpu", gpu_utilization)
-				#addDatapoint("ram", ram_percent)
-				#addDatapoint("gpu_temp", gpu_temp)
-				##addDatapoint("cpu_temp", cpu_temp) # todo add cpu_temp to hardwareinfo
-				#addDatapoint("nic_io", nic_io)
-#				#datapoints = {"cpu": [], "gpu": [], "ram": [], "nic": [], "cpu_temp": [], "gpu_temp": [], "nic_io": []}
-
-			#---> ['conmanager', ('recvdata', 'ifd', 'intermetry', b"hardwareinfo:b'd\\xa3(\\xf3\\xb7\\x85*\\x1a':50.0|[54.5, 44.4, 55.6, 54.5, 50.0, 50.0, 55.6, 45.5, 50.0, 70.0, 60.0, 58.3, 45.5, 44.4, 33.3, 36.4]|34.9|16719659008|12.0|519.0|6.33544921875|49.0|lo=127.0.0.1,enp5s0=192.168.2.166,ztrf2vegnr=192.168.192.101,ztr2qy64cl=10.147.20.101,anbox0=192.168.250.1|lo=1184531/1184531,enp5s0=58362229/3353709404,enp6s0=0/0,ztrf2vegnr=15511/190354,ztr2qy64cl=15478/500,anbox0=0/0|lo=0,enp5s0=1000,enp6s0=0,ztrf2vegnr=10,ztr2qy64cl=10,anbox0=65535|lo=65536,enp5s0=1500,enp6s0=1500,ztrf2vegnr=2800,ztr2qy64cl=2800,anbox0=1500|lo=True,enp5s0=True,enp6s0=False,ztrf2vegnr=True,ztr2qy64cl=True,anbox0=False|")]
-			#print(read)
 	print("debug@requestalldata: exit. Time taken: {}".format(time.time() - time1))
 
 def makeGraph(pData, pHeight):
@@ -150,7 +103,6 @@ def cpuAll(pVar):
 		values["cpu_all"]
 	except:
 		return False
-#	print("debug@cpupercent: WIP")
 	ramps = ""
 	for n in values["cpu_all"]:
 		n = float(n)
@@ -159,54 +111,65 @@ def cpuAll(pVar):
 	pVar.set(ramps)
 
 def gpuPercent(pVar):
-
 	global values
 	gpuID = 0 # Change this if you have multiple GPUs and it isn't picking up the correct one. Default: 0
 	try:
-		values["gpu"]
+		gpuBusy = values["gpu"][gpuID]["sensors"]["gpu_busy_percent"]
 	except:
 		return False
-	pVar.set("{:.2f}%".format(int(values["gpu"][gpuID]["sensors"]["gpu_busy_percent"]) / 1000))
-
-#	global gpu_utilization
-#	try:#
-#		gpu_utilization
-#	except:
-#		return False
-#	setString = "{:.1f}%".format(float(gpu_utilization))
-#	if len(setString) < 5:
-#		setString = " {}".format(setString)
-#	pVar.set(setString)
+	pVar.set("{: >2}%".format(gpuBusy))
 
 def gpuMemused(pVar):
-	global gpu_memused, gpu_memtotal
+	global values
+	gpuID = 0 # Change this if you have multiple GPUs and it isn't picking up the correct one. Default: 0
 	try:
-		gpu_memused
-		gpu_memtotal
+		vramUsed = values["gpu"][gpuID]["sensors"]["mem_info_vram_used"]
+		vramTotal = values["gpu"][gpuID]["sensors"]["mem_info_vram_total"]
 	except:
 		return False
-	pVar.set("{:d} / {:d} MB".format(int(float(gpu_memused)), int(float(gpu_memtotal))))
-
+	pVar.set("{:.0f} / {:.0f} MB".format(int(vramUsed) / 1048576, int(vramTotal) / 1048576))
+	
+def gpuMemusedPercent(pVar):
+	global values
+	gpuID = 0 # Change this if you have multiple GPUs and it isn't picking up the correct one. Default: 0
+	try:
+		vramUsed = values["gpu"][gpuID]["sensors"]["mem_info_vram_used"]
+		vramTotal = values["gpu"][gpuID]["sensors"]["mem_info_vram_total"]
+	except:
+		return False
+	pVar.set("{: >2}%".format(int(int(vramUsed) / int(vramTotal) * 100)))
 
 def gpuPercentGraph(pVar):
 	global datapoints
+	gpuID = 0 # Change this if you have multiple GPUs and it isn't picking up the correct one. Default: 0
 	try: # Make sure variable is initialized. Otherwise expect uncaught exception.
-		datapoints["gpu"]
-	except:
+		
+		datapointsGPUUtil = []
+		for datapoint in datapoints["gpu"]:
+			datapointsGPUUtil.append(datapoint[gpuID]["sensors"]["gpu_busy_percent"])
+		
+	except Exception as e:
 		return False
-	graph = makeGraph(datapoints["gpu"][-15:], 4)
+	graph = makeGraph(datapointsGPUUtil[-15:], 4)
 	pVar.set(graph)
 
-
 def ramPercent(pVar):
-	#ram_percent = data[2]
-	#ram_total = data[3]
 	global values
 	try:
 		values["ram_percent"]
 	except:
 		return False
 	pVar.set("{:.1f}%".format(float(values["ram_percent"])))
+
+def ramUsage(pVar):
+	global values
+	try:
+		values["ram_percent"]
+		values["ram_total"]
+		values["ram_used"]
+	except:
+		return False
+	pVar.set("{:.2f} / {:.2f} GB".format(int(values["ram_used"]) / 1073741824, int(values["ram_total"]) / 1073741824))
 
 def ramPercentGraph(pVar):
 	global datapoints
@@ -258,7 +221,6 @@ def nic1io(pVar, pNic = "enp5s0"):
 		up = "NaN"
 		down = "NaN"
 	pVar.set("{}▵\n{}▿".format(up, down))
-	print("debug@cpupercent: NIY")
 
 def nic2Data(pVar):
 	nic1Data(pVar, "enp6s0")
@@ -274,7 +236,7 @@ def tcpu(pVar):
 		values["sensors.temp"]
 	except:
 		return False
-	pVar.set("{:.2f}°C".format(int(values["sensors.temp"][cputempSensorgroup]["content"][cputempSensorID]["value"]) / 1000))
+	pVar.set("{:.0f}°C".format(int(values["sensors.temp"][cputempSensorgroup]["content"][cputempSensorID]["value"]) / 1000))
 
 def tgpu(pVar):
 	global values
@@ -284,14 +246,42 @@ def tgpu(pVar):
 		values["sensors.temp"]
 	except:
 		return False
-	pVar.set("{:.2f}°C".format(int(values["sensors.temp"][gputempSensorgroup]["content"][gputempSensorID]["value"]) / 1000))
+	pVar.set("{:.0f}°C".format(int(values["sensors.temp"][gputempSensorgroup]["content"][gputempSensorID]["value"]) / 1000))
+
+def tgpuMem(pVar):
+	global values
+	gputempSensorgroup = "amdgpu"
+	gputempSensorID = "mem"
+	try:
+		values["sensors.temp"]
+	except:
+		return False
+	pVar.set("{:.0f}°C".format(int(values["sensors.temp"][gputempSensorgroup]["content"][gputempSensorID]["value"]) / 1000))
+
+def tgpuPPT(pVar):
+	global values
+	gputempSensorgroup = "amdgpu"
+	gputempSensorID = "PPT"
+	try:
+		values["sensors.power"]
+	except:
+		return False
+	pVar.set("{} W".format(int(values["sensors.power"][gputempSensorgroup]["content"][gputempSensorID]["value"]) / 1000000))
 
 def tsys(pVar):
-	print("debug@cpupercent: NIY")
+	global values
+	tempSensorgroup = "gigabyte_wmi"
+	tempSensorID = "1"
+	try:
+		values["sensors.temp"]
+	except:
+		return False
+	pVar.set("{:.0f}°C".format(int(values["sensors.temp"][tempSensorgroup]["content"][tempSensorID]["value"]) / 1000))
 
 
 
-#Convert [n] bit/s into appropriately prefixed units. I.e. 7649 becomes 7,46 Kbit/see
+
+#Convert [n] bit/s into appropriately prefixed units. I.e. 7649 becomes 7,46 Kbit/sec
 def humanreadable(size):
     byteSuffixes = ['bit/s', 'Kbit/s', 'Mbit', 'Gbit', 'Tbit', 'Pbit']
     i = 0
@@ -301,7 +291,7 @@ def humanreadable(size):
     f = ('%.2f' % size).rstrip('0').rstrip('.')
     return '{} {}'.format(f, byteSuffixes[i])
 
-#Convert [n] bit/s into appropriately prefixed units. I.e. 7649 becomes 7,46 Kbit/see
+#Convert [n] bit/s into appropriately prefixed units. I.e. 7649 becomes 7,46 Kbit/sec
 def humanreadable1024(size):
     byteSuffixes = ['bit/s', 'KiBit/s', 'MiBit', 'GiBit', 'TiBit', 'PiBit']
     i = 0
